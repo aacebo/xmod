6878ede9ef8333bc8b13820ef518be01f1816bf1
aacebo <aacebowork@gmail.com>
2026-02-11 18:27:21 -0500

    add xval derive crate

---
+++ libs/xval-derive/src/lib.rs
@@ -0,0 +1,9 @@
+#![allow(unused)]
+
+use proc_macro::TokenStream;
+
+#[proc_macro_derive(ToValue)]
+pub fn derive_value(input: TokenStream) -> TokenStream {
+    let mut input = syn::parse_macro_input!(input as syn::DeriveInput);
+    quote::quote! {}.into()
+}
