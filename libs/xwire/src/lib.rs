c6733ae7642d454e986cfff2f1652aacb897ae75
aacebo <aacebowork@gmail.com>
2026-02-23 12:16:28 -0500

    Update lib.rs

--- libs/xwire/src/lib.rs
+++ libs/xwire/src/lib.rs
@@ -1,1 +1,35 @@
+pub trait Context<K, T: 'static> {
 
+
+    fn get(&self) -> &impl Accessor<T> {
+        self.key(std::any::type_name::<T>())
+    }
+
+    fn get_mut(&mut self) -> &mut impl Accessor<T> {
+        self.key_mut(std::any::type_name::<T>())
+    }
+}
+
+pub trait Fork<T: 'static>: Context<T> {
+    fn fork(&self) -> impl Context<T>;
+}
+
+pub trait Cancel<T: 'static>: Context<T> {
+    fn cancel();
+}
+
+///
+/// ## Accessor<T>
+/// represents a Context field/property
+/// 
+pub trait Accessor<T> {
+    fn get(&self) -> &T;
+    fn get_mut(&mut self) -> &mut T;
+    fn set(&mut self, value: T);
+}
+
+// pub trait Provider {
+//     type Target: 
+
+//     fn create(&self) -> impl Context<T>;
+// }
